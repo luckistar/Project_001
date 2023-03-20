fd321ca24edc107e15a0f41aaa8aaff0d49fc8ef
luckistar <nurbiyablat@gmail.com>
2023-03-20 14:21:29 -0700

    update project_list_005

---
+++ github_project_list_005.py
@@ -0,0 +1,7 @@
+def combine_sort(my_list1,my_list2):
+  list3=my_list1+my_list2
+  sort_list3 = sorted(list3)
+  return sort_list3
+
+#Uncomment the line below when your function is done
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
